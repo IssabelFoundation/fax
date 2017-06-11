df3de882626145c6d72faca9984e28673d5eeee3
Nicolas Gudino <asternic@gmail.com>
2017-06-11 12:30:53 -0400

    Upudate attribution comments leaving original copyright and GPL license in place

--- modules/faxviewer/libs/paloSantoFaxVisor.class.php
+++ modules/faxviewer/libs/paloSantoFaxVisor.class.php
@@ -17,7 +17,6 @@
   | the License for the specific language governing rights and           |
   | limitations under the License.                                       |
   +----------------------------------------------------------------------+
-  | The Original Code is: Elastix Open Source.                           |
   | The Initial Developer of the Original Code is PaloSanto Solutions    |
   +----------------------------------------------------------------------+
   $Id: paloSantoFaxVisor.class.php,v 1.1.1.1 2008/12/09 18:00:00 aflores Exp $ */
